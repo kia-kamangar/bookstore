bf69c80cad8f819c7b48823adee63de7ca5dba4d
kia kamangar <kia.kamangar@gmail.com>
2019-06-19 17:42:53 +0430

    Add app directory

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,38 @@
+class BooksController < ApplicationController
+  def new
+    @page_title = "Add New Book"
+    @book = Book.new
+    @category = Category.new
+    @author = Author.new
+    @publisher = Publisher.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.save
+
+    redirect_to books_path
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def index
+    @books = Book.all
+  end
+
+  def show
+  end
+
+  private
+    def book_params
+      params.require(:book).permit(:title, :category_id, :author_id, :publisher_id,
+       :isbn, :price, :buy, :format, :excerpt, :pages, :year, :coverpath)
+    end
+end
